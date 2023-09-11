e2f34e33f69b6ee86c55c4fe7cad20fbc72627a7
rasha <rashataher1245@gmail.com>
2023-09-11 10:33:32 +0300

    Fixed the code with one api

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ function App() {
       .then((result) => {
         setforecast(result);
        console.log(result);
+      })
+      .catch((error) => {
+        console.error('Error fetching weather data:', error);
       });
   };
 
